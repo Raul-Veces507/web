50a6b745db687add682ba0d7105b0afce492ea80
raul veces <raulveces@MacBook-Air-de-raul.local>
2023-09-29 09:51:58 -0500

    se agrega nueva funciones

--- Account/views.py
+++ Account/views.py
@@ -3,6 +3,12 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.core.mail import EmailMessage
 # Create your views here.
 
 def register(request):
@@ -20,8 +26,24 @@ def register(request):
             user=Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
             user.phone_number=phone_number
             user.save()
-            messages.success(request,'Se registrio el usuario exitosamente')
-            return redirect('register')
+            
+            current_site=get_current_site(request)
+            mail_subject=' Por favor activa tu cuenta en Riba Smith'
+            body= render_to_string('accounts/account_verification_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user)
+            })
+
+            to_email=email
+            send_email=EmailMessage(mail_subject,body,to=[to_email])
+            send_email.send()
+
+
+            #messages.success(request,'Se registrio el usuario exitosamente')
+
+            return redirect('/account/login?command=verification&email='+email)
 
 
 
@@ -29,7 +51,7 @@ def register(request):
     context={
         'form':form
     }
-    return render(request, 'accounts/register.html',context)
+    return render(request, 'account/register.html',context)
 
 def login(request):
     if request.method== 'POST':
@@ -48,8 +70,29 @@ def login(request):
 
 
     return render(request, 'accounts/login.html')
+
+
 @login_required(login_url='login')
 def logout(request):
     auth.logout(request)
     messages.success(request, 'Has salido de sesion')
     return redirect('login')
+
+
+def activate(request,uidb64,token):
+    try:
+        uid=urlsafe_base64_decode(uidb64).decode()
+        user=Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError,Account.DoesNotExist):
+        user=None
+
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active=True
+        user.save()
+        messages.success(request, 'Felicidades tu cuenta esta activa!')
+        return redirect('login')
+    
+    else:
+        messages.error(request,' La activacion es invalida')
+        return redirect('register')
+     
--- ribasmith/settings.py
+++ ribasmith/settings.py
@@ -157,6 +157,13 @@ MESSAGE_TAGS={
     messages.ERROR:'danger'
 }
 
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='jetveces@gmail.com'
+EMAIL_HOST_PASSWORD='kcpghbionvunfmhj'
+EMAIL_USE_TLS=True
+
+EMAIL_USE_TLS=True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
