402802d75be54dd8c41bc32cb042269eb27aa8b9
alejo50711 <alejoibarra141@gmail.com>
2023-10-27 15:47:56 -0500

    lista favoritos terminada

--- static/src/js/javascrip.js
+++ static/src/js/javascrip.js
@@ -245,7 +245,7 @@ $(document).ready(function () {
     resultsHtml += '                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>'
     resultsHtml += '                </svg>'
     resultsHtml += '            </div>'
-    resultsHtml += '            <input type="text" id="table-search-users" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar Producto">'
+    resultsHtml += '            <input type="text" id="table-search-users" class=" w-full block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg  bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar Producto">'
     resultsHtml += '        </div>'
     resultsHtml += '    </div>'
     resultsHtml += '<div  id="Marcaschecbock">'
